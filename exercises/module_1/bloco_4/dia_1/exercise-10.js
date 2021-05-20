3c5ae2007dee5ccac89d93e7dd91304bd9ee6de7
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-20 13:24:06 -0300

    Adiciona exercício 10

---
+++ exercises/module_1/bloco_4/dia_1/exercise-10.js
@@ -0,0 +1,16 @@
+let custo = 500;
+let venda = 1000;
+let imposto = 20;
+
+let impostoSoreCusto = 20 * custo / 100;
+let custoTotal = custo + impostoSoreCusto;
+let lucro = venda - custoTotal;
+
+let lucro1000 = 1000 * lucro;
+
+if (custo > 0 && venda > 0) {
+  console.log ('Se o produto for vendido mil vezes, o vendedor terá lucro de ' + lucro1000 + 'R$');
+}else if (custo < 0 || venda <0){
+  console.log('Valor inserido inválido'); 
+}
+  
