aa82b0ea84d6f653565c7abde50d131546343a22
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-20 13:26:08 -0300

    Refatora exercício 10

--- exercises/module_1/bloco_4/dia_1/exercise-10.js
+++ exercises/module_1/bloco_4/dia_1/exercise-10.js
@@ -6,7 +6,7 @@ let impostoSoreCusto = 20 * custo / 100;
 let custoTotal = custo + impostoSoreCusto;
 let lucro = venda - custoTotal;
 
-let lucro1000 = 1000 * lucro;
+let lucro1000 = venda * lucro;
 
 if (custo > 0 && venda > 0) {
   console.log ('Se o produto for vendido mil vezes, o vendedor terá lucro de ' + lucro1000 + 'R$');
