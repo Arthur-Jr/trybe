b3cf3213ac2c34c853681f1428d494ad6e7f8237
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-20 11:20:54 -0300

    Adiciona exercício 1

---
+++ exercises/module_1/bloco_4/dia_1/exercise-1.js
@@ -0,0 +1,17 @@
+let num1 = 10;
+let num2 = 5;
+
+let soma = num1 + num2;
+console.log("O resultado é igual a " + soma);
+
+let sub = num1 - num2;
+console.log("O resultado é igual a " + sub);
+
+let multiplicacao = num1 * num2;
+console.log("O resultado é igual a " + multiplicacao);
+
+let divisao = num1 / num2;
+console.log("O resultado é igual a " + divisao);
+
+let modulo = num1 % num2;
+console.log("O resultado é igual a " + modulo);
