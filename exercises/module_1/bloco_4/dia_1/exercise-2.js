5a632d55c53b2c70ea4ef5d56a0739e623c588a8
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-20 11:30:37 -0300

    Adiciona exercício 3

--- exercises/module_1/bloco_4/dia_1/exercise-2.js
+++ exercises/module_1/bloco_4/dia_1/exercise-2.js
@@ -2,9 +2,9 @@ let num1 = 6;
 let num2 = 5;
 
 if (num1 > num2) {
-  console.log(num1);
+  console.log("O maior valor é " + num1);
 }else if (num2 > num1) {
-  console.log(num2);
+  console.log("O maior valor é " + num2);
 }else{
   console.log("Os números tem o mesmo valor.")
 }
---
+++ exercises/module_1/bloco_4/dia_1/exercise-3.js
@@ -0,0 +1,13 @@
+let num1 = 8;
+let num2 = 5;
+let num3 = 3;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("O maior valor é " + num1);
+}else if (num2 > num1 && num2 > num3) {
+  console.log("O maior valor é " + num2);
+}else if (num3 > num1 && num3 > num2) {
+  console.log("O maior valor é " + num3)
+}else{
+  console.log("Os números tem o mesmo valor.")
+}
