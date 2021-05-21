0fc8c3c0e751b878294acdfd433e43fb88c5efa7
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-20 21:51:15 -0300

    Adiciona exercício 2

--- exercises/module_1/bloco_4/dia_2/exercise-1.js
+++ exercises/module_1/bloco_4/dia_2/exercise-1.js
@@ -3,3 +3,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index of numbers) {
   console.log(index);
 }
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos 
+// com a função console.log() ;
