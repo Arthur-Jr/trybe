8b637e9a9ee5542620d8ef29551a7be08bc7a576
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-20 22:25:49 -0300

    Adiciona exercício 5

--- exercises/module_1/bloco_4/dia_2/exercise-04.js
+++ exercises/module_1/bloco_4/dia_2/exercise-04.js
@@ -12,3 +12,7 @@ if (resultado > 20) {
 }else {
   console.log('Valor igual ou menos que 20!')
 }
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, 
+// imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem:
+//  "valor menor ou igual a 20";
