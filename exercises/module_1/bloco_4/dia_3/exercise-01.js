5e5ca11f285502fbb1bfe73dc7aa4c2b36d11200
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-22 17:36:04 -0300

    Adciona exercício 2 .

--- exercises/module_1/bloco_4/dia_3/exercise-01.js
+++ exercises/module_1/bloco_4/dia_3/exercise-01.js
@@ -1,13 +1,13 @@
 let n = 5;
-let resultado = '';
+let result = '';
 
 for (line = 1; line <= n; line += 1) {
-  resultado += '*' ;
+  result += '*' ;
 }  
 for (repeat = 0; repeat < n; repeat +=1) {
-console.log(resultado);
+console.log(result);
 }
 
 // O programa deve imprimir na tela um quadrado feito de asteriscos de lado do tamanho do valor da 
-//váriavel n
+// váriavel n.
  
---
+++ exercises/module_1/bloco_4/dia_3/exercise-02.js
@@ -0,0 +1,9 @@
+let n = 5;
+let result = '';
+
+for (line = 1; line <= n; line += 1) {
+  result += '*';
+  console.log(result);
+}
+
+//  O programa imprime um triângulo retângulo com 5 asteriscos de base.
