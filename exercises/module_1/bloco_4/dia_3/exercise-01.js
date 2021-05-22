af9d6f209b5871e232e5967a7648d6573344de7f
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-22 17:29:00 -0300

    Adciona dia 3 do bloco 4.

---
+++ exercises/module_1/bloco_4/dia_3/exercise-01.js
@@ -0,0 +1,13 @@
+let n = 5;
+let resultado = '';
+
+for (line = 1; line <= n; line += 1) {
+  resultado += '*' ;
+}  
+for (repeat = 0; repeat < n; repeat +=1) {
+console.log(resultado);
+}
+
+// O programa deve imprimir na tela um quadrado feito de asteriscos de lado do tamanho do valor da 
+//váriavel n
+ 
