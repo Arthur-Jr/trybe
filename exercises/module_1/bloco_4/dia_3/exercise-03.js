c5641b410e1168326a8235a5d5b81b75b5600692
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-22 23:37:19 -0300

    Adciona exercício 3.

---
+++ exercises/module_1/bloco_4/dia_3/exercise-03.js
@@ -0,0 +1,19 @@
+let n = 10;
+let result = '';
+let space = '';
+let symbol = '';
+
+for (line = 1; line <= n; line += 1) {
+  symbol += '*';
+  for (index = 0; index < n; index += 1) {
+    if (index < n - line){
+      space += '/';
+    }else if (index > n - line) {
+      break;
+    }else{
+      result = space + symbol;
+    }
+  }
+  console.log(result);
+  space = '';
+}
