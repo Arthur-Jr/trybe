424ce4d9006644add693353c444cd471411ff4c1
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-24 21:27:51 -0300

    Adiciona exercício 1.

---
+++ exercises/module_1/bloco_4/dia_4/exercise-01.js
@@ -0,0 +1,34 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log('Bem-vinda, ' + info.personagem)
+
+info['recorrente'] = 'sim';
+
+console.log(info);
+
+for (let key in info) {
+  console.log(key);
+}
+console.log(' ')
+
+for (let key in info) {
+  console.log(info[key]);
+}
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'sim',
+};
+
+console.log(' ')
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+console.log(info.recorrente + ' e ' + info2.recorrente); 
+
