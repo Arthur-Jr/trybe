46f1fa4468ff6d432bad3c4759023792f4131b8b
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-28 21:20:59 -0300

    Adiciona exercício 8.

--- exercises/module_1/bloco_5/dia_2/script.js
+++ exercises/module_1/bloco_5/dia_2/script.js
@@ -1,29 +1,35 @@
 let body = document.querySelector('body');
-function creatTag (type, father, classN) {
+function createTag (type, father, classN) {
   let tag = document.createElement(type);
   father.appendChild(tag);
   tag.className = classN;
 }
 
 //h1 title
-creatTag('h1', body, 'title');
+createTag('h1', body, 'title');
 document.querySelector('.title').innerText = 'Exercício 5.2 - JavaScript DOM';
 
 //div main-content
-creatTag('div', body, 'main-content');
+createTag('div', body, 'main-content');
 let mainContent = document.querySelector('.main-content'); 
 
 //div center-content
-creatTag('div', mainContent, 'center-content');
+createTag('div', mainContent, 'center-content');
 let centerContent = document.querySelector('.center-content')
-creatTag('p', centerContent, 'textP');
+createTag('p', centerContent, 'textP');
 centerContent.firstChild.innerText = 'Aprendendo DOM.'
 
 //div left-content
-creatTag('div', mainContent, 'left-content');
-creatTag('img', mainContent.firstChild.nextSibling, 'small-image')
+createTag('div', mainContent, 'left-content');
+createTag('img', mainContent.firstChild.nextSibling, 'small-image')
 document.querySelector('.small-image').src= 'https://picsum.photos/200';
-
+// Informei me como colocar o src no link: https://stackoverflow.com/questions/11722400/programmatically-change-the-src-of-an-img-tag
 
 //div right-content
-creatTag('div', mainContent, 'right-content');
+createTag('div', mainContent, 'right-content');
+createTag('ul', mainContent.lastChild, 'ul');
+let list = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'nove', 'dez'];
+for(let listIndex = 0; listIndex < list.length; listIndex += 1) {
+  createTag('li', mainContent.lastChild.firstChild, 'li');
+}
+
