cffe5f633d12b8650354c03fafd2943fb91a75fd
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-28 21:26:27 -0300

    Adiciona exercício 9.

--- exercises/module_1/bloco_5/dia_2/script.js
+++ exercises/module_1/bloco_5/dia_2/script.js
@@ -5,27 +5,27 @@ function createTag (type, father, classN) {
   tag.className = classN;
 }
 
-//h1 title
+//body/ h1 
 createTag('h1', body, 'title');
 document.querySelector('.title').innerText = 'Exercício 5.2 - JavaScript DOM';
 
-//div main-content
+//body/ div 
 createTag('div', body, 'main-content');
 let mainContent = document.querySelector('.main-content'); 
 
-//div center-content
+//main-content/ div  center-content
 createTag('div', mainContent, 'center-content');
 let centerContent = document.querySelector('.center-content')
 createTag('p', centerContent, 'textP');
 centerContent.firstChild.innerText = 'Aprendendo DOM.'
 
-//div left-content
+//main-content/ div  left-content
 createTag('div', mainContent, 'left-content');
 createTag('img', mainContent.firstChild.nextSibling, 'small-image')
 document.querySelector('.small-image').src= 'https://picsum.photos/200';
 // Informei me como colocar o src no link: https://stackoverflow.com/questions/11722400/programmatically-change-the-src-of-an-img-tag
 
-//div right-content
+//main-content/ div  right-content
 createTag('div', mainContent, 'right-content');
 createTag('ul', mainContent.lastChild, 'ul');
 let list = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'nove', 'dez'];
@@ -33,3 +33,7 @@ for(let listIndex = 0; listIndex < list.length; listIndex += 1) {
   createTag('li', mainContent.lastChild.firstChild, 'li');
 }
 
+// main-content/ h3 h3
+createTag('h3', mainContent, 'h3');
+createTag('h3', mainContent, 'h3');
+createTag('h3', mainContent, 'h3');
