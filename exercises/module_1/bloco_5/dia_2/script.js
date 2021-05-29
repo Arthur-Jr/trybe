a41cd835dfde011015d4cbac129507b43cc0a726
Arthur-Jr <arthurjr1906@gmail.com>
2021-05-28 21:01:09 -0300

    Adiciona exercício 5 e 6.

--- exercises/module_1/bloco_5/dia_2/script.js
+++ exercises/module_1/bloco_5/dia_2/script.js
@@ -15,5 +15,12 @@ let mainContent = document.querySelector('.main-content');
 
 //div center-content
 creatTag('div', mainContent, 'center-content');
-creatTag('p', mainContent.firstChild, 'textP');
-mainContent.firstChild.firstChild.innerText = 'Aprendendo DOM.'
+let centerContent = document.querySelector('.center-content')
+creatTag('p', centerContent, 'textP');
+centerContent.firstChild.innerText = 'Aprendendo DOM.'
+
+//div left-content
+creatTag('div', mainContent, 'left-content');
+
+//div right-content
+creatTag('div', mainContent, 'right-content');
