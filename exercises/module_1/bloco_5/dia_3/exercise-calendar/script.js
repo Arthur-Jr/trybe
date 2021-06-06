bf76fd4b05627c3f19d7c8753652c7ee53319419
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-06 17:08:42 -0300

    Adiciona parte 1 do exercício.

--- exercises/module_1/bloco_5/dia_3/exercise-calendar/script.js
+++ exercises/module_1/bloco_5/dia_3/exercise-calendar/script.js
@@ -1,4 +1,17 @@
-function createDaysOfTheWeek() {
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+function setHolidayFriday(dateList) {
+  for (let index = 0; index < dateList.children.length; index += 1) {
+    if (index === 25 || index === 26 || index === 32) {
+      dateList.children[index].classList.add('holiday'); 
+    }
+    if (index === 5 || index === 12 || index === 19 || index === 26) {
+      dateList.children[index].classList.add('friday');
+    }  
+  }
+}
+
+window.onload = function start() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
 
@@ -8,9 +21,20 @@ function createDaysOfTheWeek() {
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
-};
+  }
 
-createDaysOfTheWeek();
+  const dateList = document.querySelector('#days');
+  
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const dateListItem = document.createElement('li');
+    dateListItem.className = 'day';
+    dateListItem.innerText = dezDaysList[index];
+
+    dateList.appendChild(dateListItem);
+  }
+
+  setHolidayFriday(dateList);
+
+};
 
 // Escreva seu código abaixo.
