fe8d76eef33a1d072d4dbbe61bb18d06462be11a
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-06 17:22:12 -0300

    Adiciona parte 2 do exercício.

--- exercises/module_1/bloco_5/dia_3/exercise-calendar/script.js
+++ exercises/module_1/bloco_5/dia_3/exercise-calendar/script.js
@@ -1,5 +1,6 @@
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
 
+// Define as sextas e os feriados.
 function setHolidayFriday(dateList) {
   for (let index = 0; index < dateList.children.length; index += 1) {
     if (index === 25 || index === 26 || index === 32) {
@@ -15,6 +16,7 @@ window.onload = function start() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
   const weekDaysList = document.querySelector('.week-days');
 
+  // Adiciona os dias da semana.
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
     const dayListItem = document.createElement('li');
@@ -23,6 +25,7 @@ window.onload = function start() {
     weekDaysList.appendChild(dayListItem);
   }
 
+  // Adiciona as datas do mês.
   const dateList = document.querySelector('#days');
   
   for (let index = 0; index < dezDaysList.length; index += 1) {
@@ -37,4 +40,13 @@ window.onload = function start() {
 
 };
 
-// Escreva seu código abaixo.
+// Cria botão com id definido.
+function createButton(string, idName, father) {
+  const tag = document.createElement('button');
+  tag.id = idName;
+  tag.innerText = string;
+  father.appendChild(tag);
+}
+
+// Criação de tags.
+createButton('Feriados', 'btn-holiday', document.querySelector('.buttons-container'));
