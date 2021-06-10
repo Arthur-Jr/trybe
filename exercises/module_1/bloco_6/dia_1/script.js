2f6c26a5f96efff1387c0d383d2b41f3a6f79ac2
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-10 19:43:43 -0300

    Refatora o código.

--- exercises/module_1/bloco_6/dia_1/script.js
+++ exercises/module_1/bloco_6/dia_1/script.js
@@ -1,74 +1,60 @@
-// Funçoes para checar os campos do forms.
-function checkName(e) {
+
+document.querySelector('#btn-submit').addEventListener('click', function(e) {
+  
+  // Checar o campo nome.
   const name = document.querySelector('#full-name');
   if (name.value.length > 40) {
     e.preventDefault()
     alert('O campo nome completo excede limite de caracteres!');
   }
-}
 
-function checkEmail(e) {
+  // Checar o campo email.
   const email = document.querySelector('#input-email');
   if (email.value.length > 50) {
     e.preventDefault()
     alert('O campo email excede limite de caracteres!');
   }
-}
 
-function checkCPF(e) {
+  // Checar o campo cpf.
   const cpf = document.querySelector('#input-cpf');
   if (cpf.value.length > 11) {
     e.preventDefault()
     alert('O campo CPF excede limite de caracteres!');
   }
-}
 
-function checkAdress(e) {
+  // Checar o campo endereço.
   const adress = document.querySelector('#input-adress');
   if (adress.value.length > 200) {
     e.preventDefault()
     alert('O campo endereço excede limite de caracteres!');
   }
-}
 
-function checkCity(e) {
+  // Checar o campo cidade.
   const city = document.querySelector('#input-city');
   if (city.value.length > 28) {
     e.preventDefault()
     alert('O campo cidade excede limite de caracteres!');
   }
-}
 
-function checkAbstract(e) {
+  // Checar o campo resumo.
   const abstract = document.querySelector('#input-abstract');
   if (abstract.value.length > 1000) {
     e.preventDefault()
     alert('O campo resumo do seu curriculo excede limite de caracteres!');
   }
-}
 
-function checkRole(e) {
+  // Checar o campo cargo.
   const role = document.querySelector('#input-role');
   if (role.value.length > 40) {
     e.preventDefault()
     alert('O campo cargo excede limite de caracteres!');
   }
-}
 
-function checkDescription(e) {
+  // Checar o campo descrição.
   const description = document.querySelector('#input-description');
   if (description.value.length > 500) {
     e.preventDefault()
     alert('O campo descrição do cargo excede limite de caracteres!');
   }
-}
 
-// Clicks do botão submit.
-document.querySelector('#btn-submit').addEventListener('click', checkName);
-document.querySelector('#btn-submit').addEventListener('click', checkEmail);
-document.querySelector('#btn-submit').addEventListener('click', checkCPF);
-document.querySelector('#btn-submit').addEventListener('click', checkAdress);
-document.querySelector('#btn-submit').addEventListener('click', checkCity);
-document.querySelector('#btn-submit').addEventListener('click', checkAbstract);
-document.querySelector('#btn-submit').addEventListener('click', checkRole);
-document.querySelector('#btn-submit').addEventListener('click', checkDescription);
+});
