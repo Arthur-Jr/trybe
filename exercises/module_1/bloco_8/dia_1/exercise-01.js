37cfdfca33f2664abd3c9d84ec4ee92c133a2edb
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-23 21:47:04 -0300

    Adiciona exercício 1.

--- exercises/module_1/bloco_8/dia_1/exercise-01.js
+++ exercises/module_1/bloco_8/dia_1/exercise-01.js
@@ -1,8 +1,35 @@
+function createEmail(value) {
+  if (typeof(value) !== 'string') {
+    throw Error ('Nome Invalido!!')
+  }
+  let newString = '';
+  for (let index = 0; index < value.length; index += 1) {
+    if (value[index] === ' ') {
+      newString += '_'
+    } else {
+      newString += value[index];
+    }
+  }
+  return `${newString}@trybe.com`;
+}
+
 const newEmployees = () => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: createEmail('Pedro Guerra'),
+    id2: createEmail('Luiza Drumond'),
+    id3: createEmail('Carla Paiva'),
   }
   return employees;
 };
+
+console.log(newEmployees());
+
+const assert = require('assert');
+
+assert.throws(() => {
+  createEmail(null);
+  createEmail(undefined);
+  createEmail([ ]);
+  createEmail({ });
+  createEmail(1);
+});
