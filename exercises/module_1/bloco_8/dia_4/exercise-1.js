9b4c8917e95daddc631b1bd0c9f083d11e72809d
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-28 20:34:12 -0300

    Adiciona exercício 1.

--- exercises/module_1/bloco_8/dia_4/exercise-1.js
+++ exercises/module_1/bloco_8/dia_4/exercise-1.js
@@ -7,7 +7,13 @@ const arrays = [
 ];
 
 function flatten() {
-  // escreva seu código aqui
+  const newArray = arrays.reduce((acc, array) => {
+    array.forEach((value) => acc.push(value));
+    return acc
+  }, []);
+  return newArray;
 }
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+
+// Trasnforma multiplos arrays em um só.
