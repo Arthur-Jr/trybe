76f29cf1486b2dfc95946c9ae90b8086d33e3406
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-07 20:27:32 -0300

    Adiciona exercício 3.

--- exercises/module_1/bloco_9/dia_2/exercise-2.js
+++ exercises/module_1/bloco_9/dia_2/exercise-2.js
@@ -3,12 +3,12 @@ const promisse = new Promise ((resolve, reject) => {
   for (let number = 1; number <= 10; number += 1) {
     numbers.push(Math.floor(Math.random() * 50));
   }
-  const newNumbers = numbers
+  const sumNumbers = numbers
     .map((number) => number * number)
     .reduce((acc, number) => acc + number);
   
-  if (newNumbers < 8000) return resolve(console.log('Promise resolvida!'));
-  reject(console.log('Promise rejeitada!'));
+  if (sumNumbers < 8000) return resolve(console.log('Promise resolvida!'));
+  reject(console.log('É mais de oito mil! Essa promise deve estar quebrada'));
 })
   .then((success) => success)
   .catch((error) => error);
