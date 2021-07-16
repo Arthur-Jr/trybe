56aecc74aaa248b2eea72c2186326567c7f4e31d
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-16 00:42:49 -0300

    Adiciona exercício 4.

---
+++ exercises/module_1/bloco_10/dia_3/exercise-04.js
@@ -0,0 +1,7 @@
+const changeCase = (string) => string.toUpperCase();
+
+const getFirstLetter = (string) => string[0];
+
+const getTogether = (string1, string2) => `${string1}${string2}`; 
+
+module.exports = { changeCase, getFirstLetter, getTogether };
