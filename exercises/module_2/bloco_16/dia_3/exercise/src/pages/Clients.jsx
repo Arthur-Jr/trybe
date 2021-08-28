e2361930a63c61244cab617b823c582a915d48ce
Arthur-Jr <arthurjr1906@gmail.com>
2021-08-28 20:52:22 -0300

    Implementa a o inicio da clients page.

--- exercises/module_2/bloco_16/dia_3/exercise/src/pages/Clients.jsx
+++ exercises/module_2/bloco_16/dia_3/exercise/src/pages/Clients.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Button from '../components/Button';
 
-export default class Clients extends React.Component {
+class Clients extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/register');
+  }
+
   render() {
+    const { login } = this.props;
+    
+    if(login.user === undefined ) {
+      return(<div><h1>Login não efetuado</h1></div>);
+    }
+
     return(
       <div>
-        Clients
+        <h1>Nenhum cliente cadastrado</h1>
+        <Button handleClick={ this.handleClick } text='Cadastrar cliente' />
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  login: state.loginReducer
+});
+
+export default connect(mapStateToProps, null)(Clients);
+
+Clients.propTypes = {
+  login: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
